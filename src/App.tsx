863fc59ca22f25b3ce48f0397866f1de88a5914a
huang5587 <junhanhuang4@gmail.com>
2023-10-29 16:45:47 +0800

    added page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+//import { useState } from 'react'
+
 import './App.css'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+const HomePage = () => (
+  <div>
+    <h1>
+      Weclome to Dart Attack
+    </h1>
+    <Link to="/score-page">Go to New Page</Link>
+  </div>
+);
+const ScorePage = () => (
+  <div>
+    <h1>Scoring</h1>
+    <Link to="/">Go back to Home</Link>
+  </div>
+);
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    <Router>
+      <Routes>
+        <Route path="/" Component={HomePage} />
+        <Route path="/score-page" Component={ScorePage} />
+      </Routes>
+    </Router>
     </>
   )
 }
