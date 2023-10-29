0fc9ca76d8754270920ffbbcda6ebbc318e95fbe
rangshah99 <rangshah99@gmail.com>
2023-10-29 17:53:44 +0800

    Changes to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,57 @@
+import React, { useState } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 
-//import { useState } from 'react'
+const HomePage = () => {
+  const [players, setPlayers] = useState(2); // Default to 2 players
+  const [gameType, setGameType] = useState('501'); // Default game type
+  const [numLegs, setNumLegs] = useState(1); // Default to 1 leg
 
-import './App.css'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+  const handleSubmit = () => {
+    // You can save the selected options to your state here
+    // For example, you can store them in a context or a global state management solution
+  };
 
-const HomePage = () => (
-  <div>
-    <h1>
-      Weclome to Dart Attack
-    </h1>
-    <Link to="/score-page">Go to New Page</Link>
-  </div>
-);
+  return (
+    <div>
+      <h1>Welcome to Dart Attack</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Number of Players:
+          <input
+            type="number"
+            value={players}
+            onChange={(e) => setPlayers(e.target.value)}
+          />
+        </label>
+        <br />
+
+        <label>
+          Game Type:
+          <select value={gameType} onChange={(e) => setGameType(e.target.value)}>
+            <option value="301">301</option>
+            <option value="501">501</option>
+            {/* Add other game types here */}
+          </select>
+        </label>
+        <br />
+
+        <label>
+          Number of Legs:
+          <input
+            type="number"
+            value={numLegs}
+            onChange={(e) => setNumLegs(e.target.value)}
+          />
+        </label>
+        <br />
+
+        <Link to="/score-page">Start Game</Link>
+      </form>
+    </div>
+  );
+};
+
 const ScorePage = () => (
   <div>
     <h1>Scoring</h1>
@@ -19,20 +59,15 @@ const ScorePage = () => (
   </div>
 );
 
-
 function App() {
-
   return (
-    <>
-
     <Router>
       <Routes>
-        <Route path="/" Component={HomePage} />
-        <Route path="/score-page" Component={ScorePage} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/score-page" element={<ScorePage />} />
       </Routes>
     </Router>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
